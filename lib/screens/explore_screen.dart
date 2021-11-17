fc68a1bbe37cf2bcd65e1fc7c63c510692402a1e
Jocaagura <oficinameltablero@gmail.com>
2021-11-17 17:55:49 -0500

    Base of code for recipes :)

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../api/mock_fooderlich_service.dart';
+import '../components/components.dart';
+import '../models/models.dart';
+
+class ExploreScreen extends StatefulWidget {
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController scrollController;
+
+  void _scrollListener() {
+    if (scrollController.offset >= scrollController.position.maxScrollExtent &&
+        scrollController.position.outOfRange) {
+      print("I'm in the bottom");
+    }
+    if (scrollController.offset <= scrollController.position.minScrollExtent &&
+        scrollController.position.outOfRange) {
+      print("I'm in the top");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+    scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(_scrollListener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return FutureBuilder(
+      // 2
+      future: mockService.getExploreData(),
+      // 3
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // 4
+        if (snapshot.connectionState == ConnectionState.done) {
+          // 5
+          return ListView(
+            // 6
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            controller: scrollController,
+            children: [
+              // 7
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              // 8
+              const SizedBox(height: 16),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
+              Container(
+                height: 400,
+                color: Colors.green,
+              ),
+            ],
+          );
+        } else {
+          // 10
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
